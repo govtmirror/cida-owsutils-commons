5805bc0fd4f7ec9950bbdd708aeee1b3968738dc
Ivan Suftin <isuftin@usgs.gov>
2015-11-18 10:30:20 -0600

    Updated to latest versions of things. Still need to test  for geotools

--- src/test/java/gov/usgs/cida/owsutils/commons/shapefile/ProjectionUtilsTest.java
+++ src/test/java/gov/usgs/cida/owsutils/commons/shapefile/ProjectionUtilsTest.java
@@ -9,11 +9,14 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 /**
  *
